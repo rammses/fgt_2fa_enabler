53f737292ea0583d8fdc27d29252576dd75b4dee
rammses <mesutbayrak@gmail.com>
2020-11-13 11:02:18 +0300

    first commit & Push

---
+++ main.py
@@ -0,0 +1,88 @@
+import json
+import paramiko, time
+
+from ldap3 import Server, \
+    Connection, \
+    AUTO_BIND_NO_TLS, \
+    SUBTREE, \
+    ALL_ATTRIBUTES
+
+def get_ldap_group_members(group_name):
+    parsed_output=[]
+    with Connection(Server('172.21.21.158'),
+                    auto_bind=AUTO_BIND_NO_TLS,
+                    read_only=True,
+                    check_names=True,
+                    user='test\\administrator', password='12qwasZX') as c:
+        c.search(search_base='CN='+group_name+',OU=test1,DC=test,DC=local',
+                search_filter='(objectClass=group)',
+                search_scope='SUBTREE',
+                attributes = ['member'])
+
+    response_new= json.loads(c.response_to_json())
+    members_of_group= response_new['entries'][0]['attributes']['member']
+    for item in members_of_group:
+        parsed_output.append(item.split(',')[0].strip('CN='))
+    return parsed_output
+
+def get_ldap_mobile_phone_detail(samAccount_name):
+    with Connection(Server('172.21.21.158'),
+                    auto_bind=AUTO_BIND_NO_TLS,
+                    read_only=True,
+                    check_names=True,
+                    user='test\\administrator', password='12qwasZX') as c:
+        c.search('OU=test1,DC=test,DC=local',
+                 "(&(objectClass=person)(sAMAccountName=" + samAccount_name + "))",
+                 SUBTREE,
+                 attributes=['Mobile'])
+        result=c.response_to_json()
+        # print(result)
+        items_of_user = json.loads(result)
+        mobile_number = (items_of_user['entries'][0]['attributes']['mobile'])
+
+    return mobile_number
+
+
+# initiate connection to fw
+IP = '172.21.21.158'
+username = 'admin'
+password = '12qwasZX'
+
+client1 = paramiko.SSHClient()
+client1.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client1.connect(IP, username=username, password=password)
+configure = client1.invoke_shell()
+
+
+#bir AD grubu içindeki userları al
+users = get_ldap_group_members('2fa_members')
+
+for user in users:
+    try:
+        mobile_number = (user, get_ldap_mobile_phone_detail(user))
+        commands = ['config user local'+'\n',
+                    'edit '+user+'\n',
+                    'set type ldap'+'\n',
+                    'set two-factor sms'+'\n',
+                    'set sms-server custom'+'\n',
+                    'set sms-custom-server "sms.textapp.net"'+'\n',
+                    'set sms-phone '+str(mobile_number[1])+'\n',
+                    'set ldap-server "dc1"'+'\n',
+                    'next'+'\n',
+                    'end'+'\n'
+                    'config user group'+'\n',
+                    'edit "sslvpn_2fa"'+'\n',
+                    'append member '+str(user)+'\n',
+                    'next'+'\n',
+                    'end'+'\n',
+                   ]
+        for command in commands:
+            print("sending :", command)
+            configure.send(command)
+            time.sleep(0.5)
+            for item in str(configure.recv(65535)).split("\\r\\n"):
+                print("output", item)
+    except KeyError as k:
+        print(user, " kullanıcısının Telefon numarası yok, eklemiyoruz !")
+
+client1.close()
---
+++ readme.md
@@ -0,0 +1,26 @@
+# Helper for sms based 2 factor authentication on fortigates
+Besides having configuring
+
+- an ldap server
+- a mail server for system
+- an sms provider
+
+you'll also need to enable 2 factor with ldap on fortigates you need to ;
+
+
+- create a group that identifies that the user is a 2fa user
+- add mobile number to them
+- go into cli and enable a custom servers
+- add them to a group 
+- enable 2 factor for that particular user
+
+You need to do the second part every time you need a 2fa enabled user and it takes too much time.
+
+This script gets user info from an AD group, 
+- creates the ldap based user
+- adds the user to the sslvpn group 
+- appends the necessary config (mobile phone, sms server details)
+
+all you need to run it every 5 min, 
+
+@todo i need to add a config check so it won't add the same configuration every time.
